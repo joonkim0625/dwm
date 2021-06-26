54974154f52d5472ec94572dc51be94e28d5052b
Joon kim <joonkim0625@gmail.com>
2021-06-26 18:30:55 -0500

    apply movestack

--- config.h
+++ config.h
@@ -62,7 +62,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
   { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
